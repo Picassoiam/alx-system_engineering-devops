61ad516956d752cfdac28ff67e4ae29c849da5b8
eddie <eddie@yahoo.com>
2024-02-08 00:26:27 -0800

    tcp_and_udp_ports

--- 0x07-networking_basics/4-TCP_and_UDP_ports.sh
+++ 0x07-networking_basics/4-TCP_and_UDP_ports.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-# Use netstat to list listening sockets with PID and program name
-netstat -tulpn | awk '{print $1, $4, $7}'
+# Only show listening sockets
+# -t: TCP connections
+# -u: UDP connections
+# -l: Listening sockets
+# -p: Process ID (PID) and name
+# -n: Numerical addresses instead of resolving hostnames
+ss -t -u -l -p -n
+
+# Display headers if you want them
+# echo "Protocol\tPort\tPID\tProgram Name"
+
+# Loop through each line of output
+while read -r protocol port pid name; do
+  # Skip header line
+  if [[ "$protocol" == "Protocol" ]]; then
+    continue
+  fi
+
+  # Remove extra spaces and format output
+  printf "%-9s %-6s %-5s %s\n" "$protocol" "$port" "$pid" "$name"
+done
