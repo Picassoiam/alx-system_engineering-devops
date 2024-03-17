cca7eb4c7db55848f6872c12493734f274e1a21a
eddie <eddie@yahoo.com>
2024-03-17 05:31:21 -0700

    change_your_home_IP

--- 0x08-networking_basics_2/0-change_your_home_IP.sh
+++ 0x08-networking_basics_2/0-change_your_home_IP.sh
@@ -6,6 +6,6 @@
 # 3. Copy the hosts.new file to the /etc/hosts file
 
 cp /etc/hosts ~/hosts.new
-echo "127.0.0.2 localhost" >> ~/hosts.new
-echo "8.8.8.8  facebook.com" >> ~/hosts.new
+127.0.0.2 localhost >> ~/hosts.new
+8.8.8.8  facebook.com >> ~/hosts.new
 cp -f ~/hosts.new /etc/hosts
