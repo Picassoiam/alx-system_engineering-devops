5f5478483b348f28bddc628102f82fb2ee4b9e07
eddie <eddie@yahoo.com>
2024-03-17 04:51:06 -0700

    show_attached_ip_address

--- 0x08-networking_basics_2/1-show_attached_IPs.sh
+++ 0x08-networking_basics_2/1-show_attached_IPs.sh
@@ -3,4 +3,4 @@
 # Script tp display all IPV4 IPS on a machine
 # It prints the IP address of the machine.
 # Displays all acive IPv4 addresses.
-ifconfig | grep -Eo "inet (addr:)?([0-9]*\.){3}[0-9]*" | cut -b 11-
+ifconfig | grep -E "inet (addr:)?([0-9]*\.){3}[0-9]*" | cut -b 11-
